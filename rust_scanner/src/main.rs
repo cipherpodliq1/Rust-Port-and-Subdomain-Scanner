2d60ca1d00fc250be0e2bd2eb09f2d82fb79ca9d
cipherpodliq1 <148264989+cipherpodliq1@users.noreply.github.com>
2024-11-03 12:48:31 +0300

    Updating main.rs

--- rust_scanner/src/main.rs
+++ rust_scanner/src/main.rs
@@ -1,5 +1,24 @@
 
+use rayon::prelude::*;
 
-fn main() -> Result<() , anyhow::Error> {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    //we use a custom thread pool to improve speed
+    let pool = rayon::ThreadPoolBuilder::new().num_threads(256).build().unwrap();
+    
+    //pool.install is required to use our custom threadpool, instead of 
+    // rayon's default one
+
+    pool.install(|| {
+        let scan_result : Vec<Subdomain> =
+        subdomains::enumerate(&http_client , target).unwrap().into_par_iter().map(subdomains::scan_ports).collect();
+
+        for subdomain in scan_result {
+            println!("{}", &subdomain.domain);
+            for port in &subdomain.open_ports {
+                println!("         {}", port.port);
+            }
+            println!("");
+        }
+
+    });   
 }
